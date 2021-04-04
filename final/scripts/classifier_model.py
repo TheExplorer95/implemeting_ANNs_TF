4de0ad760f7c6ce382915d558ab9ee6d3058078f
Spinkk <mpink@uos.de>
2021-04-04 03:49:10 +0200

    linear classifier

--- final/scripts/classifier_model.py
+++ final/scripts/classifier_model.py
@@ -6,16 +6,7 @@ import numpy as np
 def get_classifier(c_dim, num_classes, reduce_model):
     embedding_inputs = tf.keras.Input(shape=(c_dim))
     x = reduce_model.get_embeddings(embedding_inputs)  # first reduce dim
-    x = tf.keras.layers.LayerNormalization()(x)
-    x = tf.keras.layers.Dense(64, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
-    x = tf.keras.layers.Dense(128, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
-    x = tf.keras.layers.Dense(256, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
-    x = tf.keras.layers.Dense(512, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.1)(x)
-
+    
     outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
     model = tf.keras.Model(
         inputs=embedding_inputs, outputs=outputs, name="music_classifier"
